d73296efbd4f1748950c2a6874363f0f0a91ad41
no-la <nzyc0216@icloud.com>
2025-11-02 12:48:38 +0900

    add collect sound, movide files

--- scripts/update-posts.js
+++ scripts/update-posts.js
@@ -6,12 +6,22 @@ require("dotenv").config();
 
 const SOURCE_DIR = process.env.POSTS_SOURCE_DIR;
 const IMAGE_SOURCE_DIR = process.env.IMAGE_SOURCE_DIR;
+const SOUND_SOURCE_DIR = process.env.SOUND_SOURCE_DIR;
+const MOVIE_SOURCE_DIR = process.env.MOVIE_SOURCE_DIR;
 if (!SOURCE_DIR) {
   console.error("Not Found SOURCE_DIR env");
   process.exit(1);
 }
 if (!IMAGE_SOURCE_DIR) {
   console.error("Not Found IMAGE_SOURCE_DIR env");
+  process.exit(1);
+}
+if (!SOUND_SOURCE_DIR) {
+  console.error("Not Found SOUND_SOURCE_DIR env");
+  process.exit(1);
+}
+if (!MOVIE_SOURCE_DIR) {
+  console.error("Not Found MOVIE_SOURCE_DIR env");
   process.exit(1);
 }
 const DEST_DIR = "posts"; // NOTE: must be same with config.DEST_DIR
@@ -24,19 +34,34 @@ const getMdDatas = (filePath) => {
   return matter(fileContent);
 };
 const isValidSlug = (slug) => /^[a-z0-9-]+$/.test(slug);
+const sourceWikiLinksRegex = (exts) => {
+  // EX: exts = ["png", "jpg", "gif"]
+  return new RegExp(`!\\[\\[(.+?)\\.(${exts.join("|")})\\]\\]`, "gi");
+};
 const collectImageFiles = (content) => {
-  const matches = content.matchAll(/\!\[\[(.+?)\.(png|jpe?g)\]\]/gi);
+  collectSourceFiles(content, ["png", "jpg", "jpeg", "gif"], IMAGE_SOURCE_DIR);
+};
+const collectSoundFiles = (content) => {
+  collectSourceFiles(content, ["mp3", "wav"], SOUND_SOURCE_DIR);
+};
+const collectMovieFiles = (content) => {
+  collectSourceFiles(content, ["mp4", "mov", "avi"], MOVIE_SOURCE_DIR);
+};
+const collectSourceFiles = (content, exts, sourceDir) => {
+  const matches = content.matchAll(sourceWikiLinksRegex(exts));
 
   for (const match of matches) {
     const p1 = match[1];
     const ext = match[2];
     const parts = p1.split("|");
     const fileName = `${parts[0]}.${ext}`;
-    const srcPath = path.join(IMAGE_SOURCE_DIR, fileName);
+    const srcPath = path.join(sourceDir, fileName);
     const destPath = path.join(POST_ASSET_DEST_DIR, encodeForURI(fileName));
 
+    console.log(`Copying ${srcPath} to ${destPath}`);
+
     if (!fs.existsSync(srcPath)) {
-      console.warn(`Not Found: Image ${srcPath} is not found.`);
+      console.warn(`Not Found: source directory ${srcPath} is not found.`);
       continue;
     }
     if (fs.existsSync(destPath)) {
@@ -46,7 +71,7 @@ const collectImageFiles = (content) => {
     const { size } = fs.statSync(srcPath);
     const sizeMB = size / (1024 * 1024);
     if (sizeMB > 1) {
-      console.warn(`Large image (${sizeMB.toFixed(2)} MB): ${srcPath}`);
+      console.warn(`Large file (${sizeMB.toFixed(2)} MB): ${srcPath}`);
     }
 
     fs.mkdirSync(path.dirname(destPath), { recursive: true });
@@ -94,6 +119,9 @@ const main = () => {
     titleToSlug[title] = data.slug;
 
     collectImageFiles(content);
+    collectSoundFiles(content);
+    collectMovieFiles(content);
+
     fs.cpSync(srcPath, destPath, { recursive: true });
   });
 
